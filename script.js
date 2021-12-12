3bff9e1e7f9b3ad689d3bcbe7885915e78373555
joaquin <jecorrea@protonmail.com>
2021-12-12 13:06:25 -0500

    Full Script using async/await

--- script.js
+++ script.js
@@ -3,18 +3,38 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 jokeBtn.addEventListener("click", getAJoke);
 
-function getAJoke(params) {
-  // Dad jokes needs a header formatted like config to access a joke.
-  //Otherwise it'll give back HTML
+//USING async/await
+async function getAJoke() {
+  //Dad jokes needs a header formatted like config to GET a joke.
+  //Otherwise it'll give back HTML as a response
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  //   console.log(data);
+  jokeEl.innerHTML = data.joke;
 }
+
+//USING .then()
+// function getAJoke() {
+//Dad jokes needs a header formatted like config to access a joke.
+//Otherwise it'll give back HTML
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//console.log(data)
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
