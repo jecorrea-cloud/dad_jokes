9aa8aec08b3a1341d3d6584e33c8fe1c6f6c49c5
joaquin <jecorrea@protonmail.com>
2021-12-12 12:49:17 -0500

    Full Script using fetch

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 jokeBtn.addEventListener("click", getAJoke);
 
-function getAJoke(params) {}
+function getAJoke(params) {
+  // Dad jokes needs a header formatted like config to access a joke.
+  //Otherwise it'll give back HTML
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}
